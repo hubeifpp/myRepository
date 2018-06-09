dfd4643ac98b3cec6e61b7024a9e412be72f6c8a
fengpingping <fengpingping@ddjf.com.cn>
2018-06-09 15:34:25 +0800

    MavenTest

---
+++ mavenTest/src/main/java/com/shenzhen/ping/mavenTest/App.java
@@ -0,0 +1,13 @@
+package com.shenzhen.ping.mavenTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
